11ecf324075c01f6904e5c5987de028ed9744020
Aaron Coburn <acoburn@amherst.edu>
2016-05-02 13:25:39 -0400

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fcrepo-camel-tests
-Integration testing of camel-based projects
+
+This project is used to test the deployment of Fedora artifacts in an OSGi runtime. In particular,
+it tests that `fcrepo-camel` and `fcrepo-camel-toolbox` deploy and run properly in a Karaf container.
+
+##Maintainers
+
+  * [Aaron Coburn](https://github.com/acoburn)
+
---
+++ src/test/java/org/fcrepo/camel/tests/FcrepoCamelIT.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.camel.tests;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+
+import java.io.File;
+
+import javax.inject.Inject;
+
+import org.apache.karaf.features.FeaturesService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.ConfigurationManager;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.slf4j.Logger;
+
+/**
+ * @author Aaron Coburn
+ * @since May 2, 2016
+ */
+@RunWith(PaxExam.class)
+public class FcrepoCamelIT {
+
+    private static Logger LOG = getLogger(FcrepoCamelIT.class);
+
+    @Inject
+    protected FeaturesService featuresService;
+
+    @Configuration
+    public Option[] config() {
+        final ConfigurationManager cm = new ConfigurationManager();
+        final String rmiRegistryPort = cm.getProperty("karaf.rmiRegistry.port");
+        final String rmiServerPort = cm.getProperty("karaf.rmiServer.port");
+        final String sshPort = cm.getProperty("karaf.ssh.port");
+        return new Option[] {
+            karafDistributionConfiguration()
+                .frameworkUrl(maven().groupId("org.apache.karaf").artifactId("apache-karaf")
+                        .versionAsInProject().type("zip"))
+                .unpackDirectory(new File("target", "exam"))
+                .useDeployFolder(false),
+            logLevel(LogLevel.WARN),
+            keepRuntimeFolder(),
+            configureConsole().ignoreLocalConsole(),
+            features(maven().groupId("org.apache.karaf.features").artifactId("standard")
+                        .versionAsInProject().classifier("features").type("xml"), "scr"),
+            features(maven().groupId("org.apache.camel.karaf").artifactId("apache-camel")
+                        .versionAsInProject().classifier("features").type("xml"), "camel-blueprint", "camel-spring"),
+            features(maven().groupId("org.fcrepo.camel").artifactId("fcrepo-camel")
+                        .versionAsInProject().classifier("features").type("xml"), "fcrepo-camel"),
+            editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", rmiRegistryPort),
+            editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", rmiServerPort),
+            editConfigurationFilePut("etc/org.apache.karaf.shell.cfg", "sshPort", sshPort),
+
+       };
+    }
+
+    @Test
+    public void testInstallation() throws Exception {
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("camel-core")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("fcrepo-camel")));
+    }
+}
